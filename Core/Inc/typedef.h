53ddfcbc0914eaafaffb612f7575ee520ed3d652
DimkaK-GIT <_dimka_@list.ru>
2021-04-30 15:56:11 +0300

    ddd

---
+++ Core/Inc/typedef.h
@@ -0,0 +1,14 @@
+#ifndef __TYPEDEF_H
+#define __TYPEDEF_H
+
+typedef struct
+			{
+				uint8_t count;		// счетчик PWM
+				uint8_t countMax;   // период PWM, максимальный счетчик 
+				uint8_t enable;     // разрешение
+				uint8_t state; 	    // состояние
+				uint8_t value;		// значение PWM
+			}pwmStateHeader;
+
+
+#endif /* __TYPEDEF_H */
